6c372b652aeee4f292ddb21d2ee0f6042d615fb5
yerunjie <yerunjie@yerunjiedeMacBook-Pro.local>
2017-09-05 21:03:21 +0800

    Change some files

--- app/src/main/java/com/quickcanteen/quickcanteen/activities/userinformation/UserInformation.java
+++ app/src/main/java/com/quickcanteen/quickcanteen/activities/userinformation/UserInformation.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -74,6 +75,7 @@ public class UserInformation extends BaseActivity {
         public void run() {
             try {
                 BaseJson baseJson = userAction.getCurrentUserInfo();
+                Log.d("ReturnCode",baseJson.getReturnCode());
                 userInfo = new UserInfoBean(baseJson.getJSONObject());
                 handler.post(new Runnable() {
                     @Override
